1e40ae5c32cb63800a83fc763714a41ac733f81d
Idiyatov <ildarthegreat@gmail.com>
2021-09-17 19:57:13 +0400

    1.3 Создать класс Point (точка) с тремя публичными неизменяемыми (final) координатами-полями типа double: x, y и z. Создать единственный конструктор, который требует задания всех трёх координат. Создать несколько точек.

--- src/main/java/ru/ssau/tk/ildar/Practice/Main.java
+++ src/main/java/ru/ssau/tk/ildar/Practice/Main.java
@@ -1,12 +1,7 @@
 package ru.ssau.tk.ildar.Practice;
-import  ru.ssau.tk.ildar.Practice.Person;
-
+import ru.ssau.tk.ildar.Practice.Point;
 class Main{
     public static void main(String[] args) {
-        Person person=new Person();
-        Person personTwo=new Person(9234);
-        Person personThree=new Person("Валерия","Атапина");
-        Person personFour=new Person("Екатерина","Арьева",1342);
-        personFour.getLastName();
+Point point = new Point(15,14,27);
     }
 }
