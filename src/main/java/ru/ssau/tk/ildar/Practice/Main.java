7ea55009b84e65d5149dd3d4882762710fe7fbcf
Idiyatov <ildarthegreat@gmail.com>
2021-09-17 21:10:39 +0400

    1.5 Добавить классу Points метод enlarge(), принимающий на вход точку Point и число с плавающей точкой double и возвращающий новую точку, полученную произведением координат входной точки на входное число.

--- src/main/java/ru/ssau/tk/ildar/Practice/Main.java
+++ src/main/java/ru/ssau/tk/ildar/Practice/Main.java
@@ -6,5 +6,7 @@ System.out.println("Сумма 24 и 34 = " + Point.sum(24,34));
         System.out.println("Разность 14 и 34 = " + Point.substract(14,34));
         System.out.println("Деления 22 на 3 = " + Point.divide(22,3));
         System.out.println("Умножения 4 на 3 = " + Point.multiply(4,3));
+        Point point = new Point();
+        System.out.println( point.enlarge(point,4));
     }
 }
--- src/main/java/ru/ssau/tk/ildar/Practice/Point.java
+++ src/main/java/ru/ssau/tk/ildar/Practice/Point.java
@@ -1,7 +1,16 @@
 package ru.ssau.tk.ildar.Practice;
 
 public class Point {
-   private Point(){}
+    private double point;
+   public Point(){
+       this.point=3;
+   }
+    public double enlarge(Point pointOne,double pointTwo){
+       return pointOne.getPoint()*pointTwo;
+    }
+    public double getPoint(){
+       return this.point;
+    }
     public static double sum(double pointOne,double pointTwo){
        return pointOne+pointTwo;
     }
