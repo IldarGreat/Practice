3876ae7d98a2452f7497b36acb7c0c3422d87a82
Idiyatov <ildarthegreat@gmail.com>
2021-09-26 20:59:56 +0400

    2.2 Создать метод, генерирующий массив чисел по входной размерности. Массив должен быть заполнен единицами, кроме первого и последнего элемента – на их месте должны быть двойки. Метод должен возвращаться созданный массив. Покрыть метод тестами.

--- src/main/java/ru/ssau/tk/ildar/Practice/WorkingWithArrays.java
+++ src/main/java/ru/ssau/tk/ildar/Practice/WorkingWithArrays.java
@@ -2,11 +2,30 @@ package ru.ssau.tk.ildar.Practice;
 
 public class WorkingWithArrays {
 
+    public static void showingTheResult(int[] array) {
+        for (int i : array) {
+            System.out.println(i);
+        }
+    }
+
     public static int[] returningArrayOfAGivenDimension(int dimension) {
         return new int[dimension];
     }
 
+    public static int[] returningArrayFilledWithOnes(int dimension) {
+        int[] array = new int[dimension];
+        for (int i : new int[]{0, dimension - 1}) {
+            array[i] = 2;
+        }
+        for (int element = 1; element < dimension - 1; element++) {
+            array[element] = 1;
+        }
+        return array;
+    }
+
     public static void main(String[] args) {
-        returningArrayOfAGivenDimension(5)[0] = 0;
+        showingTheResult(returningArrayOfAGivenDimension(5));
+        System.out.println();
+        showingTheResult(returningArrayFilledWithOnes(7));
     }
 }
--- src/test/java/ru/ssau/tk/ildar/Practice/WorkingWithArraysTest.java
+++ src/test/java/ru/ssau/tk/ildar/Practice/WorkingWithArraysTest.java
@@ -2,7 +2,8 @@ package ru.ssau.tk.ildar.Practice;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import static ru.ssau.tk.ildar.Practice.WorkingWithArrays.returningArrayOfAGivenDimension;
+import static ru.ssau.tk.ildar.Practice.WorkingWithArrays.*;
+
 
 public class WorkingWithArraysTest {
 
@@ -10,6 +11,16 @@ public class WorkingWithArraysTest {
     public static void testReturningArrayOfAGivenDimension(){
         Assert.assertEquals(returningArrayOfAGivenDimension(5).length,5);
         Assert.assertEquals(returningArrayOfAGivenDimension(9).length,9);
+    }
+
+    @Test
+    public static void testReturningArrayFilledWithOnes(){
+        int[] array = returningArrayFilledWithOnes(10);
+        Assert.assertEquals(array[0],2);
+        Assert.assertEquals(array[9],2);
+        for(int element=1;element<8;element++){
+            Assert.assertEquals(array[element],1);
+        }
     }
 
 }
