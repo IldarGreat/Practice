2e9eb2e2223891c0ff24f35146288055c82bdbb9
Idiyatov <ildarthegreat@gmail.com>
2021-10-14 10:04:17 +0400

    3.1 Создать метод, принимающий на вход строку String, и выводящий в консоль каждый символ входной строки на отдельной строке консоли. Реализовать метод с помощью цикла for, а также с использованием методов строк length() и charAt(). Нумерация символов, как и в массиве, с нуля.

---
+++ src/main/java/ru/ssau/tk/ildar/Practice/SpecialStrings.java
@@ -0,0 +1,10 @@
+package ru.ssau.tk.ildar.Practice;
+
+public class SpecialStrings {
+
+    public static void characterByCharacterOutput(String string) {
+        for (int element = 0; element < string.length(); element++) {
+            System.out.println(string.charAt(element));
+        }
+    }
+}
---
+++ src/test/java/ru/ssau/tk/ildar/Practice/SpecialStringsTest.java
@@ -0,0 +1,11 @@
+package ru.ssau.tk.ildar.Practice;
+
+import org.testng.annotations.Test;
+
+public class SpecialStringsTest {
+
+    @Test
+    public static void showCharacterByCharacterOutput(){
+        SpecialStrings.characterByCharacterOutput("Its just a test");
+    }
+}
