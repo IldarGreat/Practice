36ee7a3dfe7315c17acb7f536dba1a9b1ba0b32b
Idiyatov <ildarthegreat@gmail.com>
2021-11-21 14:40:40 +0400

    Half Closure Полузакрытие передачи данных. Прервана передача данных из одного стороны сокета , но в тоже время продолжается прием данных от другой стороны.(Наипростейший пример)

---
+++ src/main/java/ru/ssau/tk/ildar/Practice/network/HalfClosure.java
@@ -0,0 +1,23 @@
+package ru.ssau.tk.ildar.Practice.network;
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class HalfClosure {
+
+    public static void main(String[] args) throws IOException {
+        try(ServerSocket server = new ServerSocket(8189)){
+            try(Socket socket = server.accept()){
+                try(Scanner in = new Scanner(socket.getInputStream())){
+                    PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+                    out.println("We need information");
+                    socket.shutdownOutput();
+                    while(in.hasNextLine()){
+                        System.out.println("We get information");
+                    }
+                }
+            }
+        }
+    }
+}
