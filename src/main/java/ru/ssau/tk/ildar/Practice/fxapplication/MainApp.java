3314e8d8a82ecd06acc8a4333f1827014dabaf19
Idiyatov <ildarthegreat@gmail.com>
2021-11-27 13:28:18 +0400

    Импортирована в maven JavaFX и создан класс расширяющий Application

---
+++ src/main/java/ru/ssau/tk/ildar/Practice/fxapplication/MainApp.java
@@ -0,0 +1,13 @@
+package ru.ssau.tk.ildar.Practice.fxapplication;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
