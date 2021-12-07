0dcf56d7f4b227e0366f9c6b8cf775a9b3188984
Ildar <ildarthegreat@gmail.com>
2021-12-07 22:05:07 +0400

    [My Stardew Game] Создан метод run,в котором созданы все условия для того чтобы на разных устройствах приложение обновлялось одинаково

--- src/main/java/ru/ssau/tk/ildar/Practice/games/stardew/Game.java
+++ src/main/java/ru/ssau/tk/ildar/Practice/games/stardew/Game.java
@@ -2,6 +2,10 @@ package ru.ssau.tk.ildar.Practice.games.stardew;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBuffer;
+import java.awt.image.DataBufferInt;
 import java.io.Serial;
 import java.io.Serializable;
 
@@ -13,6 +17,10 @@ public class Game extends Canvas implements Runnable, Serializable {
     public static final int SCALE = 3;
     public static final String NAME = "Stardew birthday!";
     private final JFrame frame;
+    private boolean running = false;
+    public int ticksCount = 0;
+    private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+    private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
 
     public Game() {
         setMinimumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
@@ -30,8 +38,78 @@ public class Game extends Canvas implements Runnable, Serializable {
         frame.setVisible(true);
     }
 
+    public synchronized void start() {
+        running = true;
+        new Thread(this).start();
+    }
+
+    public synchronized void stop() {
+        running = false;
+    }
+
+    /**
+     * Обновляет игру , все внутренние переменные , логику игры
+     */
+    public void tick() {
+        ticksCount++;
+    }
+
+    public void render() {
+        BufferStrategy bs = getBufferStrategy();
+        if(bs ==null){
+            createBufferStrategy(3);
+            return;
+        }
+        Graphics graphics = bs.getDrawGraphics();
+        graphics.setColor(Color.BLACK);
+        graphics.fillRect(0,0,getWidth(),getHeight());
+        graphics.dispose();
+        bs.show();
+    }
+
+    /**
+     * В разных системах этот while будет работать по разному,
+     * это означает что игра на разных системах будет обновлятся с разной скоростью.
+     * Поэтому нужно ограничить эту скорость в 60 обновлений в секунду
+     */
     @Override
     public void run() {
+        long lastTime = System.nanoTime();
+        double nsPerTick = 1_000_000_000D / 60D; // Как много нс находится в tick
+        int ticks = 0;
+        int frames = 0;
+        long lastTimer = System.currentTimeMillis();
+        double delta = 0;
+        while (running) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / nsPerTick;
+            lastTime = now;
+            boolean shouldRender = true;
+            while (delta >= 1) {
+                ticks++;
+                tick();
+                delta--;
+                shouldRender = true;
+            }
+            try {
+                Thread.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 
+            frames++;
+            render();
+
+            if (System.currentTimeMillis() - lastTimer >= 1000) {
+                lastTimer += 1000;
+                System.out.println("frames:" + frames + ",ticks: " + ticks);
+                frames = 0;
+                ticks = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new Game().start();
     }
 }
